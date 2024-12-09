0a9671f1f60ef39ffe413ac6aa0f1b242e175037
ben-Chermside <bchermsi@oberlin.edu>
2024-12-08 22:34:11 -0500

    adding

--- importData.py
+++ importData.py
@@ -1,8 +1,21 @@
-import mysql.connector
+# import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="your_host",
-  user="your_user",
-  password="your_password",
-  database="your_database"
-)
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="bchermsi",
+#   password="50177BenC",
+#   database="bchermsi"
+# )
+
+import pandas as pd
+import sqlalchemy
+
+# Read the CSV file into a Pandas DataFrame
+df = pd.read_csv('Musium.csv')
+
+# Create an SQLAlchemy engine to connect to the database
+engine = sqlalchemy.create_engine('mysql+pymysql://bchermsi:50177BenC@localhost:3306/bchermsi')
+
+# Write the DataFrame to the database
+df.to_sql('your_table_name', engine, index=False, if_exists='replace')
+
