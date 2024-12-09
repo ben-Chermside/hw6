e22de0d3df41cfcf94e0224b7bf29a410f96f28b
ben-Chermside <bchermsi@oberlin.edu>
2024-12-09 00:43:09 -0500

    a

--- get.php
+++ get.php
@@ -31,8 +31,8 @@ $textget = $_POST["textget"];
 $Description = $_POST["Description"];
 $ALLInfoGet = $_POST["ALL"];
 
-$infoGetsCheckboxes = array($IDget, $Accessionget, $TypeGet, , $Acquisitionget, $Artistget, $Titleget, $Object_Namegetget, $Techniquesget, $Dimensionsget, $Descriptionget, $Cultureget, $Dynastyget, $textget, $Description);
-$getCorrospondingName = array("ObjectID","Accession_Number","Classification","Acquisition_Method","Artist_Maker","Title","Object_Name","Materials_Techniques","Dimensions","Description","Culture","Period_Dynasty","Department","eMuseum_Label_Text");
+// $infoGetsCheckboxes = array($IDget, $Accessionget, $TypeGet, , $Acquisitionget, $Artistget, $Titleget, $Object_Namegetget, $Techniquesget, $Dimensionsget, $Descriptionget, $Cultureget, $Dynastyget, $textget, $Description);
+// $getCorrospondingName = array("ObjectID","Accession_Number","Classification","Acquisition_Method","Artist_Maker","Title","Object_Name","Materials_Techniques","Dimensions","Description","Culture","Period_Dynasty","Department","eMuseum_Label_Text");
 
 print("line 2");
 
