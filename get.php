7139181c772c3aaf9e0c3976fd7621bc70275a70
ben-Chermside <bchermsi@oberlin.edu>
2024-12-09 00:33:40 -0500

    updated php file

--- get.php
+++ get.php
@@ -6,18 +6,85 @@ $dbName         = "bchermsi";
 $user           = "bchermsi";
 $pw             = "50177BenC";
 
-$name = $_POST["name"];
 $ID = $_POST["ID"];
-$csci = $_POST["csci"];
-$finance = $_POST["finance"];
-$music = $_POST["music"];
-$physics = $_POST["physics"];
-$history = $_POST["history"];
-$bio = $_POST["biology"];
-$elecEng = $_POST["elecEng"];
-$all = $_POST["ALL"];
-$minSalary = $_POST["minSalary"];
-$maxSalary = $_POST["maxSalary"];
+$Accession = $_POST["Accession"];
+$IDget = $_POST["IDget"];
+$Accessionget = $_POST["Accessionget"];
+$TypeGet = $_POST["TypeGet"];
+$Acquisitionget = $_POST["Acquisitionget"];
+$Artistget = $_POST["Artistget"];
+$Titleget = $_POST["Titleget"];
+$Object_Namegetget = $_POST["Object_Namegetget"];
+$Techniquesget = $_POST["Techniquesget"];
+$Dimensionsget = $_POST["Dimensionsget"];
+$Descriptionget = $_POST["Descriptionget"];
+$Cultureget = $_POST["Cultureget"];
+$Dynastyget = $_POST["Dynastyget"];
+$textget = $_POST["textget"];
+$Description = $_POST["Description"];
+$ALLInfoGet = $_POST["ALL"];
+
+$infoGetsCheckboxes = array($IDget, $Accessionget, $TypeGet, , $Acquisitionget, $Artistget, $Titleget, $Object_Namegetget, $Techniquesget, $Dimensionsget, $Descriptionget, $Cultureget, $Dynastyget, $textget, $Description);
+$getCorrospondingName = array(
+"ObjectID",
+"Accession_Number",
+"Classification",
+"Acquisition_Method",
+"Artist_Maker",
+"Title",
+"Object_Name",
+"Materials_Techniques",
+"Dimensions",
+"Description",
+"Culture",
+"Period_Dynasty",
+"Department",
+"eMuseum_Label_Text"
+);
+
+
+
+$Asian = $_POST["Asian"];
+$European = $_POST["European"];
+$American = $_POST["American"];
+$Indian = $_POST["Indian"];
+$African = $_POST["African"];
+$Modern = $_POST["Modern"];
+$ALLDepartment = $_POST["ALLDepartment"];
+$artest = $_POST["artest"];
+$Title = $_POST["Title"];
+$ObjectName = $_POST["Object Name"];
+$year = $_POST["year"];
+$Techniques = $_POST["Techniques"];
+$CreditLine = $_POST["Credit Line"];
+$keywords = $_POST["keywords"];
+$Edo = $_POST["Edo"];
+$Meiji = $_POST["Meiji"];
+$Ming = $_POST["Ming"];
+$Qing = $_POST["Qing"];
+$Showa = $_POST["Showa"];
+$allDinastys = $_POST["allDinastys"];
+$dinastys = $_POST["dinastys"];
+$Bequest = $_POST["Bequest"];
+$found = $_POST["found"];
+$Gift = $_POST["Gift"];
+$Purchase = $_POST["Purchase"];
+$Rental = $_POST["Rental"];
+$otherAquisition = $_POST["otherAquisition"];
+$allAquisition = $_POST["allAquisition"];
+
+
+
+// $csci = $_POST["csci"];
+// $finance = $_POST["finance"];
+// $music = $_POST["music"];
+// $physics = $_POST["physics"];
+// $history = $_POST["history"];
+// $bio = $_POST["biology"];
+// $elecEng = $_POST["elecEng"];
+// $all = $_POST["ALL"];
+// $minSalary = $_POST["minSalary"];
+// $maxSalary = $_POST["maxSalary"];
 
 
 
@@ -39,76 +106,105 @@ function PrintPage($body, $year) {
 //ini_set('display_startup_errors', '1');
 //error_reporting(E_ALL);
 
-
-
-if($csci === "on"){
-    $stm = $stm . " OR dept_name= 'Comp. Sci.'";
-}
-
- $conn = new PDO("mysql:host=$serverName;dbname=$dbName",
-	 $user = "bchermsi", $pw = "50177BenC");
+$conn = new PDO("mysql:host=$serverName;dbname=$dbName",
+         $user = "bchermsi", $pw = "50177BenC");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$insSTMT = 
-	//$instSTMT = $conn->prepare('INSERT INTO instructor (ID, name, dept_name, salary) VALUES (:id, :name, :dept, :salary);');
+$insSTMT =
+        //$instSTMT = $conn->prepare('INSERT INTO instructor (ID, name, dept_name, salary) VALUES (:id, :name, :dept, :salary);');
 $queryResult = null;
-$stm = "SELECT * FROM instructor WHERE 1=1";
+
+
+$stm = "FROM instructor WHERE 1=1";
+if ($ALLInfoGet === "on"){
+	$stm = "SELECT * FROM instructor WHERE 1=1";
+}
+else{
+	$stm = "FROM instructor WHERE 1=1"
+	for ($i=0; $i<count($infoGetsCheckboxes); $i++){
+		if($infoGetsCheckboxes[$i] === "on"){
+			$stm = 	getCorrospondingName[$i] . ", " . $stm;
+		}
+	}
+	$stm = "SELECT " . $stm;
+
+}
 
 $subArray = array();
-if ($name === "ALL" OR $name === ""){
+
+// $subArray = array();
+
+
+
+
+// if($csci === "on"){
+//     $stm = $stm . " OR dept_name= 'Comp. Sci.'";
+// }
+
+//  $conn = new PDO("mysql:host=$serverName;dbname=$dbName",
+// 	 $user = "bchermsi", $pw = "50177BenC");
+// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// $insSTMT = 
+// 	//$instSTMT = $conn->prepare('INSERT INTO instructor (ID, name, dept_name, salary) VALUES (:id, :name, :dept, :salary);');
+// $queryResult = null;
+// $stm = "SELECT * FROM instructor WHERE 1=1";
+
+// $subArray = array();
+// if ($name === "ALL" OR $name === ""){
     
-}
-else{
-	$stm = $stm . " AND name=:name";
-	$subArray[':name'] = $name;
-}
-if($ID ==="" OR $ID === "ALL"){
+// }
+// else{
+// 	$stm = $stm . " AND name=:name";
+// 	$subArray[':name'] = $name;
+// }
+// if($ID ==="" OR $ID === "ALL"){
 
-}
-else{
-	$stm = $stm . " AND ID =:id";
-	$subArray[':id'] = $ID;
-}
-if($all === "on"){
-}
-else{
-	$stm = $stm . " (1=0";
-	if($csci === "on"){
-	    $stm = $stm . " OR dept_name= 'Comp. Sci.'";
-	}
+// }
+// else{
+// 	$stm = $stm . " AND ID =:id";
+// 	$subArray[':id'] = $ID;
+// }
+// if($all === "on"){
+// }
+// else{
+// 	$stm = $stm . " (1=0";
+// 	if($csci === "on"){
+// 	    $stm = $stm . " OR dept_name= 'Comp. Sci.'";
+// 	}
 
-	if($finance === "on"){
-	    $stm = $stm . " OR dept_name= 'Finance'";
-	}
-	if($music === "on"){
-	    $stm = $stm . " OR dept_name= 'Music'";
-	}
-	if($physics === "on"){
-	    $stm = $stm . " OR dept_name= 'Physics'";
-	}
-	if($history === "on"){
-	    $stm = $stm . " OR dept_name= 'Physics'";
-	}
-	if($biology === "on"){
-	    $stm = $stm . " OR dept_name= 'Biology'";
-	}
-	if($elecEng === "on"){
-	    $stm = $stm . " OR dept_name= 'Elec. Eng.'";
-	}
-	$stm = $stm . ")";
-}
-if($maxSalary == ""){
-}
-else{
-	$stm = $stm . " AND salary<:max";
-	$subArray[':max'] = $maxSalary;
-}
-if($minSalary == ""){
+// 	if($finance === "on"){
+// 	    $stm = $stm . " OR dept_name= 'Finance'";
+// 	}
+// 	if($music === "on"){
+// 	    $stm = $stm . " OR dept_name= 'Music'";
+// 	}
+// 	if($physics === "on"){
+// 	    $stm = $stm . " OR dept_name= 'Physics'";
+// 	}
+// 	if($history === "on"){
+// 	    $stm = $stm . " OR dept_name= 'Physics'";
+// 	}
+// 	if($biology === "on"){
+// 	    $stm = $stm . " OR dept_name= 'Biology'";
+// 	}
+// 	if($elecEng === "on"){
+// 	    $stm = $stm . " OR dept_name= 'Elec. Eng.'";
+// 	}
+// 	$stm = $stm . ")";
+// }
+// if($maxSalary == ""){
+// }
+// else{
+// 	$stm = $stm . " AND salary<:max";
+// 	$subArray[':max'] = $maxSalary;
+// }
+// if($minSalary == ""){
    
-}
-else{
-	$stm = $stm . " AND salary>:min";
-	$subArray[':min'] = $minSalary;
-}
+// }
+// else{
+// 	$stm = $stm . " AND salary>:min";
+// 	$subArray[':min'] = $minSalary;
+// }
+
 $stm = $stm . ";";
 $stmPre = $conn->prepare($stm);
 $stmPre->execute($subArray);
