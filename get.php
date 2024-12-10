3400357874227ea06ce379f8669d85110ed270a9
ben-Chermside <bchermsi@oberlin.edu>
2024-12-09 21:00:00 -0500

    a

--- get.php
+++ get.php
@@ -132,79 +132,6 @@ else{
 
 $subArray = array();
 
-// $subArray = array();
-
-
-
-
-// if($csci === "on"){
-//     $stm = $stm . " OR dept_name= 'Comp. Sci.'";
-// }
-
-//  $conn = new PDO("mysql:host=$serverName;dbname=$dbName",
-// 	 $user = "bchermsi", $pw = "50177BenC");
-// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// $insSTMT = 
-// 	//$instSTMT = $conn->prepare('INSERT INTO instructor (ID, name, dept_name, salary) VALUES (:id, :name, :dept, :salary);');
-// $queryResult = null;
-// $stm = "SELECT * FROM instructor WHERE 1=1";
-
-// $subArray = array();
-// if ($name === "ALL" OR $name === ""){
-    
-// }
-// else{
-// 	$stm = $stm . " AND name=:name";
-// 	$subArray[':name'] = $name;
-// }
-// if($ID ==="" OR $ID === "ALL"){
-
-// }
-// else{
-// 	$stm = $stm . " AND ID =:id";
-// 	$subArray[':id'] = $ID;
-// }
-// if($all === "on"){
-// }
-// else{
-// 	$stm = $stm . " (1=0";
-// 	if($csci === "on"){
-// 	    $stm = $stm . " OR dept_name= 'Comp. Sci.'";
-// 	}
-
-// 	if($finance === "on"){
-// 	    $stm = $stm . " OR dept_name= 'Finance'";
-// 	}
-// 	if($music === "on"){
-// 	    $stm = $stm . " OR dept_name= 'Music'";
-// 	}
-// 	if($physics === "on"){
-// 	    $stm = $stm . " OR dept_name= 'Physics'";
-// 	}
-// 	if($history === "on"){
-// 	    $stm = $stm . " OR dept_name= 'Physics'";
-// 	}
-// 	if($biology === "on"){
-// 	    $stm = $stm . " OR dept_name= 'Biology'";
-// 	}
-// 	if($elecEng === "on"){
-// 	    $stm = $stm . " OR dept_name= 'Elec. Eng.'";
-// 	}
-// 	$stm = $stm . ")";
-// }
-// if($maxSalary == ""){
-// }
-// else{
-// 	$stm = $stm . " AND salary<:max";
-// 	$subArray[':max'] = $maxSalary;
-// }
-// if($minSalary == ""){
-   
-// }
-// else{
-// 	$stm = $stm . " AND salary>:min";
-// 	$subArray[':min'] = $minSalary;
-// }
 
 $stm = $stm . ";";
 $stmPre = $conn->prepare($stm);
@@ -213,12 +140,13 @@ $stmPre->execute($subArray);
  $body = "<table><tr><th>ID</th><th>Name</th><th>salary</th><th>department</th></tr>";
  foreach($stmPre->fetchAll(PDO::FETCH_ASSOC) as $key =>$val ) {
 	$body .= "<tr><td>$key</td>";
-	for($addTo=0; $addTo<count($infoGetsCheckboxes); $addTo++){
-		$body .= "<tr>$val[$getCorrospondingName[$addTo]]</tr>";
+	for($addTo=0; $addTo<count($infoGetsCheckboxes); $addTo = $addTo + 1){
+		$toInsert = $val[$getCorrospondingName[$addTo]];
+		$body .= "<tr>$toInsert</tr>";
 	}
 	$body .= "</td>\n";
 
-	
+
     // $body .= "<tr><td>$key</td><td>" .
     //                $val['Title'] .
     //                //"</td><td>" . $val['salary'] . "</td><td>" . $val["dept_name"] .
