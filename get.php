f013f4f0f6c9fc3eca85b1e49c321b8a1065a185
ben-Chermside <bchermsi@oberlin.edu>
2024-12-09 21:26:13 -0500

    a

--- get.php
+++ get.php
@@ -142,10 +142,9 @@ $stmPre->execute($subArray);
 	//$body = $body . "<tr><td>$key</td>";
 	for($addTo=0; $addTo<count($infoGetsCheckboxes); $addTo = $addTo + 1){
 		if($infoGetsCheckboxes[$addTo] == "on"){
-			print("<P>entered loop</P>");
-			print("<P>$addTo</P>");
 			$toInsert = $val[$getCorrospondingName[$addTo]];
 			//prev($toInsert);
+			print("<P>$toInsert</P>");
 			$body = $body . "<tr>$toInsert</tr>";
 		}
 	}
